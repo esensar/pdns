441772d90930298961b3a910635a7b07dcf47032
Ensar Sarajčić <dev@ensarsarajcic.com>
2026-02-03 16:03:34 +0100

    Revert "redis: use copy cache exclusively"
    This reverts commit ac80fd757b16a0daa64883feafd6301c8bca5e2c.
    
    Signed-off-by: Ensar Sarajčić <dev@ensarsarajcic.com>

--- pdns/dnsdistdist/redis.cc
+++ pdns/dnsdistdist/redis.cc
@@ -602,9 +602,8 @@ bool CopyCachingRedisClient::getValue(const std::string& key, std::string& value
     return true;
   }
 
-  auto found = false;
+  auto found = d_client->getValue(key, value);
   if (d_copyCache->needsUpdate()) {
-    found = d_client->getValue(key, value);
     d_copyCache->insertBatch(d_client->generateCopyCache());
   }
   return found;
@@ -622,12 +621,7 @@ bool CopyCachingRedisClient::keyExists(const std::string& key)
   }
 
   // No value to store in the cache here, so just return
-  auto found = false;
-  if (d_copyCache->needsUpdate()) {
-    found = d_client->keyExists(key);
-    d_copyCache->insertBatch(d_client->generateCopyCache());
-  }
-  return found;
+  return d_client->keyExists(key);
 }
 
 bool FilteringCopyCachingRedisClient::getValue(const std::string& key, std::string& value)
